a00e27dde3c5d961ae7090ea0fb0bd8c9cc2e7c7
Isak Pålsson <ispalsson@gmail.com>
2021-12-07 09:32:52 +0100

    11!!!

--- Isak.py
+++ Isak.py
@@ -11,7 +11,18 @@ def start_isak():
         choise = input("""Dr.G: NO! Don't go there. We don't know what will happen, you could DIE! Now turn back and continue on course.
                        Do you want to continue?:    """)
         if "yes" in choise.lower():
-            print("""You find the item""")
-        
+            puzzle = str(input("""You find a sword laying on a table and a tablet. You read the tablet:
+                  "To gain power you must be smart, solve this puzzle and gain the 'Sword of Smiting':
+                  
+                  || |
+                  || |
+                  | ||
+                  
+                  Dr.G: It seems this is the puzzle. Look you can input three numbers. I wonder what the answer is!
+                  You can input 3 numbers like this: 'num num num'"""""))
+            puzzle.split()
+            print(puzzle)
+
+start_isak()
         
         
