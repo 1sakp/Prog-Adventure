1792cca8f0214fd866525b6aa34823427cc273ed
Isak Pålsson <ispalsson@gmail.com>
2021-12-07 08:46:42 +0100

    !!!

--- Game.py
+++ Game.py
@@ -5,5 +5,6 @@ def inv():
     print(f"You open your bag and find:  {inventory}")
     
 def attack(creature, weapon):
-    creature_hp = creature - weapon
-    print(f"You did {weapon} dmg, the {creature} has {creature_hp}HP left!")
+    creature_hp = creature[0]
+    creature_hp_new = creature_hp - weapon
+    print(f"You did {weapon} dmg, the {creature} has {creature_hp_new}HP left!")
