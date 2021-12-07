9822e712af2c5f5d51115d5d4013b03d624e824a
Isak Pålsson <ispalsson@gmail.com>
2021-12-07 09:17:45 +0100

    !!!

--- Game.py
+++ Game.py
@@ -1,10 +1,13 @@
 inventory = []
-
-
+player_hp = 100
+player_dmg = ["Fists", 1]
+rat = [10, 1]
 def inv():
     print(f"You open your bag and find:  {inventory}")
     
 def attack(creature, weapon):
     creature_hp = creature[0]
+    creature_dmg = creature[1]
     creature_hp_new = creature_hp - weapon
     print(f"You did {weapon} dmg, the {creature} has {creature_hp_new}HP left!")
+    print(f"The {creature} attacks you and does {creature_dmg} you have {player_hp}HP left!")
--- Isak.py
+++ Isak.py
@@ -1,2 +1,17 @@
 from Game import *
-
+def start_isak():
+    choise = input("""You enter contiusness and look at your suroundings.
+          You see a path to the right and the left.
+          Dr.G: Now go left and win the Trial of Champions, it won't be too hard.
+          Where do you want to go:  """)
+    if "left" in choise.lower():
+        print("""You enter what looks like an arena like that from ancient greece.
+              Dr.G: You should see a arena now, do you see it?""")
+    if "right" in choise.lower():
+        choise = input("""Dr.G: NO! Don't go there. We don't know what will happen, you could DIE! Now turn back and continue on course.
+                       Do you want to continue?:    """)
+        if "yes" in choise.lower():
+            print("""You find the item""")
+        
+        
+        
