cad23d8824d63e5e22d3e03b393561b8873d60fc
Dominik <dominik.korzepa@elev.ga.ntig.se>
2021-12-14 08:58:45 +0100

    UwU

--- Dominik.py
+++ Dominik.py
@@ -1,1 +1,5 @@
 from Game import *
+
+def introduction():
+    choice_1 = input("""Welcome D-Class-69420, You have done well completing the 1st stage.
+    I am very happy with Your progress, you have already beaten 40% of our """)
