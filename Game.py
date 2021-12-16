30d770c3d232205512cd35a119738c33cca15677
Isak Pålsson <ispalsson@gmail.com>
2021-12-16 10:24:03 +0100

    AttACK

--- Game.py
+++ Game.py
@@ -3,7 +3,7 @@ inventory = []
 inventory.append(items[1])
 player_hp = 100
 player_dmg = ["Fists", 1, "Sword of Smiting"]
-rat = [10, 1]
+rat = [10, 1, "rat"]
 weapon = {
     "fist": 1,
     "Sword of smiting":4 
@@ -13,16 +13,17 @@ def inv():
     print(f"You open your bag and find:  {inventory}")
     
 def attack(creature):
-    creature_hp = creature_hp = creature[0]
-    choise = int(input(f"You have {inventory} in your inventory. Use numbers to chose a weapon... count from the start!"))
-    choise-=1
-    weapon_local = weapon[inventory[choise]]
+    global player_hp
+    creature_hp = creature[0]
+    choise = (input(f"You have {inventory} in your inventory. Chose a weapon: use only singular:    "))
+    weapon_local = weapon[choise]
     creature_dmg = creature[1]
     while True:
         if creature_hp >= 1:
             creature_hp = creature_hp - weapon_local
-            print(f"You did {weapon} dmg, the {creature} has {creature_hp}HP left!")
-            print(f"The {creature} attacks you and does {creature_dmg} you have {player_hp}HP left!")
+            player_hp = player_hp - creature_dmg
+            print(f"You did {weapon[choise]} dmg, the {creature[2]} has {creature_hp}HP left!")
+            print(f"The {creature[2]} attacks you and does {creature_dmg} you have {player_hp}HP left!")
         else:
             print("You won!")
             return()
